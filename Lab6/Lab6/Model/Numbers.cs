b5aaa4afa4949c8155bcec5859e5bb755633f635
snowcat911 <105830293+snowcat911@users.noreply.github.com>
2022-05-19 12:09:38 +0900

    Add files via upload

---
+++ Lab6/Lab6/Model/Numbers.cs
@@ -0,0 +1,22 @@
+﻿using Lab6.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab6.Model
+{
+    class Numbers
+    {
+        public String returnResult(DateTime number1)
+        {
+            var date = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            /*date.ToShortDateString();
+            number1.ToShortDateString();*/
+            var temp = date - number1;
+            var tp = temp.Days;
+            return tp.ToString();
+        }
+
+    }
+}
---
+++ Lab6/Lab6/ViewModels/MyViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Lab6.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lab6.ViewModels
+{
+    class MyViewModel : INotifyPropertyChanged
+    {
+        
+        private string result;    
+        private DateTime selectedDate;
+
+        public DateTime SelectedDate
+        {
+            get
+            {
+                return selectedDate;
+            }
+            set
+            {
+                selectedDate = value;
+                RaisePropertyChanged("SelectedDate");
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return result;
+            }
+
+            set
+            {
+                result = value;
+                RaisePropertyChanged("Result");
+            }
+        }
+
+
+
+        private RelayCommand clickCommand;
+        public RelayCommand ClickCommand
+        {
+            get
+            {
+                return clickCommand ??
+                (clickCommand = new RelayCommand(obj =>
+                { //при нажатии на кнопку производится расчет результата 
+                    Numbers numbers = new Numbers(); //создадим объект класса модели
+                    Result = numbers.returnResult(SelectedDate); //вызовем функцию расчета результата и присвоим ее значение
+                //соответствующему полю Result
+ }));
+            }
+        }
+
+
+        private RelayCommand resetCommand;
+        public RelayCommand ResetCommand
+        {
+            get
+            {
+                return resetCommand ??
+                  (resetCommand = new RelayCommand(obj =>
+                  {
+                      Result = ""; //обнуление результата
+                  }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+
+
+        public MyViewModel() //пустой конструктор
+        {
+        }
+    }
+}
